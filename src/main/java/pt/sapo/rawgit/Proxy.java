f918b09987c125a1ca2920f95a457e47fbc775d6
brsantos <bruno-r-santos@telecom.pt>
2014-11-04 15:05:29 +0000

    first remote commit

---
+++ src/main/java/pt/sapo/rawgit/Proxy.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.sapo.rawgit;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContextBuilder;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.detect.Detector;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author brsantos
+ */
+@WebServlet("/*")
+public class Proxy extends HttpServlet
+{
+
+	private final static String ROOT = "https://raw.githubusercontent.com";
+	private static HttpClient client;
+	private static final Logger log = LoggerFactory.getLogger(Proxy.class);
+
+	static
+	{
+		try
+		{
+			SSLContextBuilder builder = new SSLContextBuilder();
+			builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());
+			Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+				.register("http", new PlainConnectionSocketFactory())
+				.register("https", sslsf)
+				.build();
+			PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
+			cm.setMaxTotal(10);
+			client = HttpClientBuilder.create().setHostnameVerifier(new AllowAllHostnameVerifier()).setConnectionManager(cm).build();
+		}
+		catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException ex)
+		{
+			log.error(null, ex);
+		}
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo()
+	{
+		return "Short description";
+	}// </editor-fold>
+
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		processRequest(request, response);
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		processRequest(request, response);
+	}
+
+	/**
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+	 *
+	 * @param request  servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException      if an I/O error occurs
+	 */
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String url = ROOT + request.getRequestURI().replace("/rawgit", "");
+		try (OutputStream out = response.getOutputStream())
+		{
+			out.write(client.execute(new HttpGet(url), rsp ->
+								 {
+									 try
+									 {
+										 TikaConfig config = TikaConfig.getDefaultConfig();
+										 Detector detector = config.getDetector();
+										 byte[] data = EntityUtils.toByteArray(rsp.getEntity());
+										 TikaInputStream stream = TikaInputStream.get(data);
+										 Metadata metadata = new Metadata();
+										 metadata.add(Metadata.RESOURCE_NAME_KEY, url);
+										 response.setContentType(detector.detect(stream, metadata).toString());
+										 return data;
+									 }
+									 catch (IOException ex)
+									 {
+										 log.error(null, ex);
+										 return new byte[0];
+									 }
+			}));
+		}
+	}
+}
