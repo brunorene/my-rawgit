5079a03b5435dfc0b564494135c55d14224301ce
brsantos <bruno-r-santos@telecom.pt>
2014-11-04 15:09:26 +0000

    changing package

--- src/main/java/pt/brunorene/rawgit/Proxy.java
+++ src/main/java/pt/brunorene/rawgit/Proxy.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pt.sapo.rawgit;
+package pt.brunorene.rawgit;
 
 import java.io.IOException;
 import java.io.OutputStream;
